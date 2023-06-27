6e97ade4effb3255ef78d6c698fd1dce2b1085de
Aleksandar Sasa <a.s.janjanin@astriigroup.com>
2023-06-27 09:30:18 +0100

    Add rough data-shaping

---
+++ data-shaping/converter/data_model_mapping.py
@@ -0,0 +1,107 @@
+SIMPLE_REFERENCE_MAPPING = {
+    "TraineeStatus": {
+        "id": {"sheet": "tlkpStatus", "column": "STID"},
+        "title": {"sheet": "tlkpStatus", "column": "STSTAT"},
+    },
+    "EmailPreference": {
+        "id": {"sheet": "tlkpEmailPref", "column": "ETID"},
+        "title": {"sheet": "tlkpEmailPref", "column": "ETPREF"},
+    },
+    "TrainingProgram": {
+        "id": {"sheet": "tlkpProgramType", "column": "PRID"},
+        "title": {"sheet": "tlkpProgramType", "column": "PRPRGM"},
+    },
+    "Salary": {
+        "id": {"sheet": "tlkpSalary", "column": "SRID"},
+        "title": {"sheet": "tlkpSalary", "column": "SRSALARY"},
+    },
+    "JobSector": {
+        "id": {"sheet": "tlkpFirstJobType", "column": "FJID"},
+        "name": {"sheet": "tlkpFirstJobType", "column": "FJTYPE"},
+    },
+    "NHSBand": {
+        "id": {"sheet": "tlkpAFCBand", "column": "BDID"},
+        "title": {"sheet": "tlkpAFCBand", "column": "BDBAND"},
+    },
+    "ContractType": {
+        "id": {"sheet": "tlkpContractType", "column": "COID"},
+        "title": {"sheet": "tlkpContractType", "column": "COTYPE"},
+    },
+    "Title": {
+        "id": {"sheet": "tlkpTitle", "column": "id"},
+        "title": {"sheet": "tlkpTitle", "column": "title"},
+    },
+    "Gender": {
+        "id": {"sheet": "tlkpGender", "column": "GNID"},
+        "title": {"sheet": "tlkpGender", "column": "GNGNDR"},
+    },
+    "Ethnicity": {
+        "id": {"sheet": "tlkpEthnic", "column": "ECID"},
+        "title": {"sheet": "tlkpEthnic", "column": "ECDESC"},
+    },
+    "DisabillityCategory": {
+        "id": {"sheet": "tlkpDisabilityCategory", "column": "DCID"},
+        "title": {"sheet": "tlkpDisabilityCategory", "column": "DCDESC"},
+    },
+    "Religion": {
+        "id": {"sheet": "tlkpReligion", "column": "RLID"},
+        "title": {"sheet": "tlkpReligion", "column": "RLREL"},
+    },
+    "SexualOrientation": {
+        "id": {"sheet": "tlkpSexor", "column": "SOID"},
+        "title": {"sheet": "tlkpSexor", "column": "SODESC"},
+    },
+    "Nationality": {
+        "id": {"sheet": "tlkpCountries", "column": "CTID"},
+        "title": {"sheet": "tlkpCountries", "column": "CTCNTR"},
+    },
+    "MaritalStatus": {
+        "id": {"sheet": "tlkpMaritalStatus", "column": "MRID"},
+        "title": {"sheet": "tlkpMaritalStatus", "column": "MRDESC"},
+    },
+    "FundingProvider": {
+        "id": {"sheet": "tlkpLETB", "column": "REID"},
+        "title": {"sheet": "tlkpLETB", "column": "RELETB"},
+    },
+    "HEIAward": {
+        "id": {"sheet": "tlkpHEI", "column": "HEID"},
+        "title": {"sheet": "tlkpHEI", "column": "HENM"},
+    },
+    "HSSTPathway": {
+        "id": {"sheet": "tlkpHSSTPathway", "column": "PTID"},
+        "title": {"sheet": "tlkpHSSTPathway", "column": "PTDESC"},
+    },
+    "Specialism": {
+        "id": {"sheet": "tlkpSpecialism", "column": "SPID"},
+        "title": {"sheet": "tlkpSpecialism", "column": "SPSPEC"},
+    },
+    "RecruitmentMethod": {
+        "id": {"sheet": "tlkpEntryType", "column": "ENID"},
+        "title": {"sheet": "tlkpEntryType", "column": "ENENTR"},
+    },
+    "Contact": {
+        "id": {},
+        "title": {},
+    },
+    "ContactType": {
+        "id": {"sheet": "tlkpContactType", "column": "NTID"},
+        "title": {"sheet": "tlkpContactType", "column": "NTTYPE"},
+    },
+    "AssessmentType": {
+        "id": {"sheet": "tlkpOSFAType", "column": "OTID"},
+        "title": {"sheet": "tlkpOSFAType", "column": "OTTYPE"},
+    },
+    "Outcome": {
+        "id": {"sheet": "tlkpOSFAType", "column": "OTID"},
+        "title": {"sheet": "tlkpOSFAType", "column": "OTTYPE"},
+    },
+    "Employer": {
+        "id": {"sheet": "tlkpTrusts", "column": "TSID"},
+        "is_nhs": {"sheet": "tlkpTrusts", "column": "TSNHS"},
+        "title": {"sheet": "tlkpTrusts", "column": "TSTRUST"},
+    },
+    "EmployerLocation": {
+        "id": {"sheet": "tlkpLETB", "column": "REID"},
+        "title": {"sheet": "tlkpLETB", "column": "RELETB"},
+    },
+}
---
+++ data-shaping/main.py
@@ -0,0 +1,25 @@
+from ingest.ingest import read_spreadsheet 
+from output.output import transfer_data, transfer_leave_of_absence_record, transfer_support_record, transfer_trainee_contact, transfer_annual_review_progression, transfer_employment_record, transfer_exit_assessment_record, transfer_trainees
+import os
+
+
+INPUT_DATA_TABLES_PATH = './data/old_model/SampleData_DataTables.xlsx'
+INPUT_REFERENCE_TABLES_PATH = './data/old_model/SampleData_ReferenceTables.xlsx'
+
+OUTPUT_DATABASE_PATH = './data/new_model/Database.xlsx'
+
+data_tables = read_spreadsheet(INPUT_DATA_TABLES_PATH)
+reference_tables = read_spreadsheet(INPUT_REFERENCE_TABLES_PATH)
+
+if os.path.exists(OUTPUT_DATABASE_PATH):
+    os.remove(OUTPUT_DATABASE_PATH)
+
+sheets = {**reference_tables, **data_tables}
+transfer_data(path=OUTPUT_DATABASE_PATH, sheets=sheets)
+transfer_leave_of_absence_record(path=OUTPUT_DATABASE_PATH, sheets=sheets)
+transfer_support_record(path=OUTPUT_DATABASE_PATH, sheets=sheets)
+transfer_trainee_contact(path=OUTPUT_DATABASE_PATH, sheets=sheets)
+transfer_annual_review_progression(path=OUTPUT_DATABASE_PATH, sheets=sheets)
+transfer_employment_record(path=OUTPUT_DATABASE_PATH, sheets=sheets)
+transfer_exit_assessment_record(path=OUTPUT_DATABASE_PATH, sheets=sheets)
+transfer_trainees(path=OUTPUT_DATABASE_PATH, sheets=sheets)
