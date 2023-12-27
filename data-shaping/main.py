092a894935a2356a57723d8fe526f64a682f24cf
Aleksandar Sasa <a.s.janjanin@astriigroup.com>
2023-12-27 12:22:00 +0000

    Add ignore-errors flag for running unitl the data is validated

--- data-shaping/main.py
+++ data-shaping/main.py
@@ -2,7 +2,12 @@ from ingest.ingest import read_spreadsheet
 from output.output import transfer_data, transfer_leave_of_absence_record, transfer_support_record, transfer_trainee_contact, transfer_annual_review_progression, transfer_employment_record, transfer_exit_assessment_record, transfer_trainees, transfer_mid_review_progression, transfer_locations
 from validator.validator import validate_dfs
 import os
+import sys
+from argparse import ArgumentParser
 
+parser = ArgumentParser()
+parser.add_argument('--ignore-errors', dest='ignore_errors', action='store_true', default=False, help='Ignore the validation errors and continue the execution of the script.')  
+args = parser.parse_args()
 
 INPUT_DATA_TABLES_PATH = 'data/old_model/SampleData_DataTables.xlsx'
 INPUT_REFERENCE_TABLES_PATH = 'data/old_model/SampleData_ReferenceTables.xlsx'
@@ -15,7 +20,7 @@ reference_tables = read_spreadsheet(INPUT_REFERENCE_TABLES_PATH)
 reference_tables_validation_success = validate_dfs(reference_tables)
 data_tables_validation_success = validate_dfs(data_tables)
 
-if not (reference_tables_validation_success and data_tables_validation_success):
+if (not (reference_tables_validation_success and data_tables_validation_success)) and not args.ignore_errors:
     exit()
 
 if os.path.exists(OUTPUT_DATABASE_PATH):
