bda8e88387ec00b03086c73375b5780fc3484bc1
Aleksandar Sasa <a.s.janjanin@astriigroup.com>
2024-03-01 11:24:55 +0000

    Add reference tables and columns checking

--- data-shaping/main.py
+++ data-shaping/main.py
@@ -9,11 +9,14 @@ from validator.schema_output import VALIDATION_SCHEMA_OUTPUT
 from validator.foreign_key_schema_output import FOREIGN_KEY_SCHEMA as FOREIGN_KEY_SCHEMA_OUTPUT
 from validator.foreign_key_schema_input import FOREIGN_KEY_SCHEMA as FOREIGN_KEY_SCHEMA_INPUT
 from validator.validator import validate_dfs, validate_foreign_keys
+from validator.comparator import compare_two_input_files
+from utils.separator import print_separator
 
 parser = ArgumentParser()
 parser.add_argument('--ignore-errors', dest='ignore_errors', action='store_true', default=False, help='Ignore the validation errors and continue the execution of the script.')
 parser.add_argument('--rows', dest='rows', action='store', default=None, help='Specify the number of rows in the Registration table to process.')
 parser.add_argument('--show-all-indices', dest='show_all_indices', action='store_true', default=False, help='Show all indices of fk columns that are not present in the fk (reference) table.')
+parser.add_argument('--compare-reference-tables', dest='ref_tables_filename', action='store', default=None, help='Specify the name of the old reference tables file to compare with, including the extension. The file is expected to be in the data/old_model/ folder and if not present, an error will be thrown.')
 args = parser.parse_args()
 
 INPUT_DATA_TABLES_PATH = 'data/old_model/SampleData_DataTables.xlsx'
@@ -24,8 +27,15 @@ OUTPUT_DATABASE_PATH = './data/new_model/Database.xlsx'
 data_tables = read_spreadsheet(INPUT_DATA_TABLES_PATH)
 reference_tables = read_spreadsheet(INPUT_REFERENCE_TABLES_PATH)
 
-def print_separator(message: str) -> None:
-    print(f"\n\n{message}\n" + ("".join(x for x in ["="]*80)) + "\n")
+comparison_successful = True
+
+# If flag passed, read and compare the old reference tables
+if args.ref_tables_filename:
+    OLD_REFERENCE_TABLES_PATH = f'data/old_model/{args.ref_tables_filename}'
+    old_reference_tables = read_spreadsheet(OLD_REFERENCE_TABLES_PATH)
+    comparison_successful = compare_two_input_files(new_tables=reference_tables, old_tables=old_reference_tables)
+
+
 
 print_separator("Shape Validation")
 shape_validation_success = validate_dfs(reference_tables | data_tables, VALIDATION_SCHEMA)
@@ -33,10 +43,13 @@ shape_validation_success = validate_dfs(reference_tables | data_tables, VALIDATI
 print_separator("Foreign Key Validation")
 fk_validation_success = validate_foreign_keys(data_tables | reference_tables, FOREIGN_KEY_SCHEMA_INPUT, show_all_indices=args.show_all_indices)
 
-if (not (shape_validation_success and fk_validation_success)) and not args.ignore_errors:
+if (not (shape_validation_success and fk_validation_success and comparison_successful)) and not args.ignore_errors:
     print_separator("Input Validation Failed")
     print("Schema validation: " + ("Passed ✅" if shape_validation_success else "Failed ❌"))
     print("Foreign keys validation: " + ("Passed ✅" if fk_validation_success else "Failed ❌"))
+    if args.ref_tables_filename:
+        print("Comparison: " + ("Passed ✅" if comparison_successful else "Failed ❌"))
+    
     exit()
 
 if os.path.exists(OUTPUT_DATABASE_PATH):
---
+++ data-shaping/validator/comparator.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from utils.separator import print_separator
+
+def are_table_names_same(new_tables: dict[str, pd.DataFrame], old_tables: dict[pd.DataFrame]) -> bool:
+    are_same = True
+    for table_name, _ in new_tables.items():
+        print("\n🔍 Checking if table: \033[0;36m" + table_name + ":\u001b[37m is in old file.")
+
+        if table_name not in old_tables:
+            are_same = False
+            print("\t❌ \033[0;31m" + table_name + "\u001b[37m not amongst old tables")
+        else:
+            print(f'\t✅ Present')
+
+        
+
+    for table_name, _ in old_tables.items():
+        print("\n🔍 Checking if table: \033[0;36m" + table_name + ":\u001b[37m is in new file.")
+        if table_name not in new_tables:
+            are_same = False
+            print("\t❌ \033[0;31m" + table_name + "\u001b[37m not amongst new tables")
+        else:
+            print(f'\t✅ Present')
+    
+    return are_same
+
+
+def are_column_names_same(new_tables: dict[str, pd.DataFrame], old_tables: dict[pd.DataFrame]) -> bool:
+    are_same = True
+    for table_name, table in new_tables.items():
+        print("\n🔍 Checking columns in table: \033[0;36m" + table_name + "\u001b[37m")
+        if table_name in old_tables:
+            new_table_columns_set = set(table.columns.values.tolist())
+            old_table_columns_set = set(old_tables[table_name].columns.values.tolist())
+
+            columns_only_present_in_new = list(new_table_columns_set - old_table_columns_set)
+            columns_only_present_in_old = list(old_table_columns_set - new_table_columns_set)
+
+            if len(columns_only_present_in_new) > 0:
+                print(f"\t❌ For table \033[0;31m{table_name}\u001b[37m, columns not present in the old file: \033[0;33m", ", ".join(columns_only_present_in_new) + " \u001b[37m")
+                are_same = False
+            
+            elif len(columns_only_present_in_old) > 0:
+                print(f"\t❌ For table \033[0;31m{table_name}\u001b[37m, columns not present in the new file: \033[0;33m", ", ".join(columns_only_present_in_old) + " \u001b[37m")
+                are_same = False
+
+            else:
+                print(f'\t✅ Columns match.')
+
+    return are_same
+
+
+def are_reference_table_values_same(new_tables: dict[str, pd.DataFrame], old_tables: dict[pd.DataFrame]) -> bool:
+    return True
+
+def compare_two_input_files(new_tables: dict[str, pd.DataFrame], old_tables: dict[pd.DataFrame]) -> bool:
+    are_same = True
+    print_separator("Comparing Reference Table Names")
+    are_same = are_table_names_same(new_tables, old_tables) and are_same
+
+    print_separator("Comparing Reference Table Column Names")
+    are_same = are_column_names_same(new_tables, old_tables) and are_same
+
+    print_separator("Comparing Reference Table Values")
+    are_same = are_reference_table_values_same(new_tables, old_tables) and are_same
+
+    return are_same
